1b2b088f5db9ccbcd0288308608fb0d37135338b
Rakesh Singh <rakeshsr6789@gmail.com>
2019-03-27 20:11:18 +0530

    Remove unwanted stuff

--- app/src/main/java/com/example/rakeshsingh/moviemate/utils/AppConstants.java
+++ app/src/main/java/com/example/rakeshsingh/moviemate/utils/AppConstants.java
@@ -1,7 +1,7 @@
 package com.example.rakeshsingh.moviemate.utils;
 
 public class AppConstants {
-    public final static String API_KEY="31ff2f1ac310aab99cd32cc7805c198c";
+    public final static String API_KEY="Paste The key here";
     public final static String BASE_URL = "https://api.themoviedb.org/3/";
     public final static String POPULAR = "popular";
     public final static String TOP_RATED = "top_rated";
